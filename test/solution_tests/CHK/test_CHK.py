f9ceaa2bb955e08ea8f877d08b37bd7cc815e960
sbx_user1051 <sbx_user1051@169.254.6.141>
2025-04-21 15:50:45 +0000

    Mon Apr 21 15:50:45 UTC 2025

--- test/solution_tests/CHK/test_CHK.py
+++ test/solution_tests/CHK/test_CHK.py
@@ -2,34 +2,13 @@ from lib.solutions.CHK.checkout_solution import CheckoutSolution
 
 class TestCheckout():
     def test_checkout(self):
-        assert CheckoutSolution.checkout(self, "AAA") == 130
-        assert CheckoutSolution.checkout(self, "AA") == 100
-        assert CheckoutSolution.checkout(self, "BB") == 45
-        assert CheckoutSolution.checkout(self, "AAAA") == 180
-        assert CheckoutSolution.checkout(self, "AAB") == 130
-        assert CheckoutSolution.checkout(self, "ABCD") == 115
-        assert CheckoutSolution.checkout(self, "AAABBCCDD") == 245
-        assert CheckoutSolution.checkout(self, "EE") == 80
-        assert CheckoutSolution.checkout(self, "EEB") == 80
-        assert CheckoutSolution.checkout(self, "EEBB") == 110
-        assert CheckoutSolution.checkout(self, "EEBBB") == 125
-        assert CheckoutSolution.checkout(self, "FF") == 20
-        assert CheckoutSolution.checkout(self, "FFF") == 20
-        assert CheckoutSolution.checkout(self, "FFFF") == 30
+        assert CheckoutSolution.checkout(self, "AAABBC") == 195
+        assert CheckoutSolution.checkout(self, "AAAAAD") == 215
+        assert CheckoutSolution.checkout(self, "EEBCD") == 115
         assert CheckoutSolution.checkout(self, "FFFFF") == 40
-        assert CheckoutSolution.checkout(self, "FFFFFF") == 40
-        assert CheckoutSolution.checkout(self, "") == 0
-        assert CheckoutSolution.checkout(self, "123") == -1
-        assert CheckoutSolution.checkout(self, None) == -1
-        assert CheckoutSolution.checkout(self, "A") == 50
-        assert CheckoutSolution.checkout(self, "C") == 20
-        assert CheckoutSolution.checkout(self, "Z") == 50
-        assert CheckoutSolution.checkout(self, "AAA") == 130
-        assert CheckoutSolution.checkout(self, "AAAAA") == 200
-        assert CheckoutSolution.checkout(self, "AAAAAA") == 250
-        assert CheckoutSolution.checkout(self, "ABCD") == 115
-        assert CheckoutSolution.checkout(self, "AAABBCCDD") == 245
-        assert CheckoutSolution.checkout(self, "EEBBB") == 125
-        assert CheckoutSolution.checkout(self, "AABCDEFGHIJKLMNOPQRSTUVWXYZ") == 1015
-        assert CheckoutSolution.checkout(self, "EENNRRFFUUVV") == 450
-        assert CheckoutSolution.checkout(self, "AAAAAABBEEHHHHHHHHHHKK") == 590
+        assert CheckoutSolution.checkout(self, "HHHHHH") == 55
+        assert CheckoutSolution.checkout(self, "NNNMM") == 135
+        assert CheckoutSolution.checkout(self, "RRRQQ") == 180
+        assert CheckoutSolution.checkout(self, "STXSTX") == 90
+        assert CheckoutSolution.checkout(self, "UUUUUU") == 200
+        assert CheckoutSolution.checkout(self, "VVVKKB") == 280
