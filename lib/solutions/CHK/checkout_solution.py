c0a5b99c8bd72a7de39352624aa50da905117914
sbx_user1051 <sbx_user1051@169.254.6.141>
2025-04-21 14:34:19 +0000

    Mon Apr 21 14:34:19 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,12 +3,12 @@ class CheckoutSolution:
     def checkout(self, skus):
 
         self.PRICES = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 
-                       'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 80, 'L': 90,
+                       'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 70, 'L': 90,
                        'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50,
-                       'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90,
-                       'Y': 10, 'Z': 50}
+                       'S': 20, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 17,
+                       'Y': 20, 'Z': 21}
         self.SPECIAL_OFFERS_DISCOUNT = {'A': [(5, 200), (3, 130)], 'B': [(2, 45)], 'F': [(3, 20)],
-                                        'H': [(10, 80), (5, 45)], 'K': [(2, 150)], 'P': [(5, 200)],
+                                        'H': [(10, 80), (5, 45)], 'K': [(2, 120)], 'P': [(5, 200)],
                                         'Q': [(3, 80)], 'U':[(4, 120)], 'V': [(3, 130), (2, 90)]}
         self.SPECIAL_OFFERS_FREE = {'E': (2, 'B'), 'N': (3, 'M'), 'R': (3, 'Q')}
 
@@ -54,3 +54,4 @@ if __name__ == "__main__":
     print(checkout.checkout("AABCDEFGHIJKLMNOPQRSTUVWXYZ"))
     print(checkout.checkout("EENNRRFFUUVV"))
     print(checkout.checkout("AAAAAABBEEHHHHHHHHHHKK"))
+
